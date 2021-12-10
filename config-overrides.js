197843128c898d63e7031037941472789ee4c7c3
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-12-10 20:35:42 +0200

    imported new framer motion library, and added webpack congig file to able styled component library work.

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+//To able styled component library work (bug solved)
+module.exports = function override(webpackConfig) {
+  webpackConfig.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: "javascript/auto",
+  });
+
+  return webpackConfig;
+};
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+const Section = styled.section`
+  height: 100hv;
+`;
+const Container = styled.div``;
+
+const Hero = () => {
+  return (
+    <Section>
+      <Container>hi</Container>
+    </Section>
+  );
+};
+
+export default Hero;
